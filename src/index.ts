923737a8174e849d4b11ab975d280231c8231470
Kpaiy <kpaiy42@gmail.com>
2024-07-21 18:03:47 +1000

    Add skeleton websocket code
    And I'll pretend to understand it entirely, yup.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import ws from 'ws';
 import express from 'express';
 
 import routes from './routes';
@@ -5,8 +6,21 @@ import routes from './routes';
 const app = express();
 const port = 3000; // TODO: .env the shit out of this.
 
+const wsServer = new ws.Server({ noServer: true });
+wsServer.on('connection', socket => {
+  console.log('connection handled');
+  socket.on('message', message => console.log(message));
+});
+
 app.use(express.json());
 
 app.use('/', routes);
 
-app.listen(port);
+const server = app.listen(port);
+
+server.on('upgrade', (request, socket, head) => {
+  console.log('ooh upgrade');
+  wsServer.handleUpgrade(request, socket, head, socket => {
+    wsServer.emit('connection', socket, request);
+  });
+});
